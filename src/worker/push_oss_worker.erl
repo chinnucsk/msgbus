aa9b953f8097fa39b9e55542064372575ae98fe5
root <root@ARM2-E6.(none)>
2012-11-29 15:38:59 +0800

    fix bug

--- src/worker/push_oss_worker.erl
+++ src/worker/push_oss_worker.erl
@@ -15,7 +15,7 @@ run(ArgTuple) ->
 	Cmd = io_lib:format("/opt/oss_python_cmd/osscmd put ~s oss://~s/~s", [FilePath, Bucket, Key]),
 	try
 		os:cmd(Cmd),
-		lager:info("Command: ~s ...............~n", [Cmd])
+		lager:info("Command: ..................~n~s~n", [Cmd])
 	catch _A:_B ->
 		lager:error("!!!Error!!!!!!!!!!!!!!!1~p!!!!~p", [_A, _B]),
 		callback(ArgTuple)
